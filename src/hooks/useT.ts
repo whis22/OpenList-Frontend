c2dbcf25aaad037b3a9ade3c7935e39f3661998f
MadDogOwner <xiaoran@xrgzs.top>
2025-07-16 10:00:50 +0800

    refactor(i18n): update to @solid-primitives/i18n v2 (#109)
    * chore(deps): update to vite@6
    
    * fix(useDownload): correct token parameter handling
    
    * chore(deps): update to typescript@5
    
    * chore(deps): update to @solid-primitives/i18n@2
    
    * feat(i18n): update usage to @solid-primitives/i18n@2
    
    * fix(useT): remove log added in dev

--- src/hooks/useT.ts
+++ src/hooks/useT.ts
@@ -1,25 +1,35 @@
-import { useI18n } from "@solid-primitives/i18n"
+import { dict, i18n, languages } from "~/app/i18n"
 import { firstUpperCase } from "~/utils"
 
-const useT = () => {
-  const [t] = useI18n()
+export const useT = () => {
+  const t = i18n.translator(dict)
+  const tt = (key: string, params?: i18n.BaseTemplateArgs) => {
+    return i18n.resolveTemplate ? i18n.resolveTemplate(key, params) : t(key)
+  }
   return (
     key: string,
-    params?: Record<string, string> | undefined,
+    params?: i18n.BaseTemplateArgs | undefined,
     defaultValue?: string | undefined,
   ) => {
-    const value = t(key, params, defaultValue)
-    if (!value) {
-      if (import.meta.env.DEV) return key
-      let lastDotIndex = key.lastIndexOf(".")
-      if (lastDotIndex === key.length - 1) {
-        lastDotIndex = key.lastIndexOf(".", lastDotIndex - 1)
-      }
-      const last = key.slice(lastDotIndex + 1)
-      return firstUpperCase(last).split("_").join(" ")
-    }
-    return value
+    const value = params
+      ? (tt(key, params) as string | undefined)
+      : (t(key) as string | undefined)
+
+    if (value) return value
+
+    if (defaultValue) return defaultValue
+
+    if (import.meta.env.DEV) return key
+
+    return formatKeyAsDisplay(key)
   }
 }
 
-export { useT }
+const formatKeyAsDisplay = (key: string): string => {
+  let lastDotIndex = key.lastIndexOf(".")
+  if (lastDotIndex === key.length - 1) {
+    lastDotIndex = key.lastIndexOf(".", lastDotIndex - 1)
+  }
+  const last = key.slice(lastDotIndex + 1)
+  return firstUpperCase(last).split("_").join(" ")
+}
